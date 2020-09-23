c724e6e079edfe3c27e4ce3b483c5111d98d2147
Jake Weinstein <jake@aospa.co>
2020-09-23 06:49:37 +0200

    Revert "msm: ADSPRPC: Fix to avoid race condition and use after free"
    This reverts commit 99d604642ea81f1596bb3734d82896da19f29ede.

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2645,7 +2645,7 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 		pr_err("adsprpc: ERROR: %s: user application %s trying to unmap without initialization\n",
 			 __func__, current->comm);
 		err = -EBADR;
-		return err;
+		goto bail;
 	}
 	mutex_lock(&fl->internal_map_mutex);
 
@@ -2693,11 +2693,6 @@ bail:
 	return err;
 }
 
-/*
- *	fastrpc_internal_munmap_fd can only be used for buffers
- *	mapped with persist attributes. This can only be called
- *	once for any persist buffer
- */
 static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 				struct fastrpc_ioctl_munmap_fd *ud)
 {
@@ -2706,15 +2701,14 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 
 	VERIFY(err, (fl && ud));
 	if (err)
-		return err;
+		goto bail;
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
 		pr_err("adsprpc: ERROR: %s: user application %s trying to unmap without initialization\n",
 			__func__, current->comm);
 		err = -EBADR;
-		return err;
+		goto bail;
 	}
-	mutex_lock(&fl->internal_map_mutex);
 	mutex_lock(&fl->map_mutex);
 	if (fastrpc_mmap_find(fl, ud->fd, ud->va, ud->len, 0, 0, &map)) {
 		pr_err("adsprpc: mapping not found to unmap fd 0x%x, va 0x%llx, len 0x%x\n",
@@ -2724,13 +2718,10 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 		mutex_unlock(&fl->map_mutex);
 		goto bail;
 	}
-	if (map && (map->attr & FASTRPC_ATTR_KEEP_MAP)) {
-		map->attr = map->attr & (~FASTRPC_ATTR_KEEP_MAP);
+	if (map)
 		fastrpc_mmap_free(map, 0);
-	}
 	mutex_unlock(&fl->map_mutex);
 bail:
-	mutex_unlock(&fl->internal_map_mutex);
 	return err;
 }
 
@@ -2749,7 +2740,7 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
 		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
 			__func__, current->comm);
 		err = -EBADR;
-		return err;
+		goto bail;
 	}
 	mutex_lock(&fl->internal_map_mutex);
 	if (ud->flags == ADSP_MMAP_ADD_PAGES) {
